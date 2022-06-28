ecbd0efb103cd8f18ec320f4b87a40f91d77fb5f
Owen <97201095+OwenPortfolio@users.noreply.github.com>
2022-06-28 17:06:26 +0100

    Update ebird.tsx
    Replace API key variable

--- assets/api/ebird.tsx
+++ assets/api/ebird.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const eBirdApi = axios.create({
   baseURL: "https://api.ebird.org/v2/",
-  headers: {'X-eBirdApiToken': '60cs4036s2ab'}
+  headers: API TOKEN
 });
 
 export const getRecentBirdsByLocation = (lat:number, lng:number) => {
