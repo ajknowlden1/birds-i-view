10c08d9baad266922556fab5ed0fd2ea5dfb1f50
OwenPortfolio <owen.manby@gmail.com>
2022-06-30 16:46:37 +0100

    add map page and heatmap overlay

--- assets/api/ebird.tsx
+++ assets/api/ebird.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { apiKeys } from './keys'
+import { apiKeys } from "../../secretKeys"
 
 const eBirdApi = axios.create({
   baseURL: "https://api.ebird.org/v2/",
--- assets/components/Map.tsx
+++ assets/components/Map.tsx
@@ -1,22 +1,74 @@
-import {View, StyleSheet, Text} from "react-native" 
+import { useState, useEffect } from "react";
+import {View, StyleSheet, Text, Dimensions} from "react-native" 
+import MapView, {PROVIDER_GOOGLE, Heatmap} from 'react-native-maps';
+import { getBirdsByLocation } from "../api/ebird";
+import * as Location from "expo-location";
 
 export default function Map(){
+    
+
+    // function getPosition(){
+    //     Location.requestForegroundPermissionsAsync();
+    //     let position = Location.getCurrentPositionAsync;
+    //     return position
+    // }
+
+    // useEffect(() => {
+    //     getPosition()
+    //     .then((res:any) => {
+    //         console.debug(res)
+    //     })
+    // })
+
+    let points = 
+        [{latitude:53.6908, longitude:-1.8191, weight:10},
+         {latitude:53.6968, longitude:-1.8191, weight:10},
+         {latitude:53.6968, longitude:-1.8191, weight:10},
+         {latitude:53.6968, longitude:-1.5491, weight:10},
+         {latitude:53.8008, longitude:-1.5491, weight:10},
+         {latitude:53.8008, longitude:-1.5491, weight:10},
+         {latitude:53.8008, longitude:-1.8107593, weight:10},
+         {latitude:53.8008, longitude:-1.3698524, weight:10},
+         {latitude:53.8008, longitude:-1.8855123, weight:10},
+         {latitude:53.8008, longitude:-1.5491, weight:10}]
     return (
-    <View
-                style={StyleSheet.container}
+            <View
+            style={styles.container}
             >
-                <Text style={StyleSheet.text}>Hello</Text>
+                <Text style={styles.text}>Sightings Nearby</Text>
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude:53.8008,
+                        longitude:-1.5491,
+                        latitudeDelta: 0.75,
+                        longitudeDelta: 0.75,
+                    }}
+                    showsUserLocation={true}
+                    provider={PROVIDER_GOOGLE}
+                    >
+                <Heatmap
+                    points={points}
+                    opacity={3}
+                    radius={45}
+                    />
+
+                </MapView>
             </View>
     )
 }
 
 const styles = StyleSheet.create({
+    map:{
+        width: Dimensions.get('window').width - 35,
+        height: Dimensions.get('window').height - 200,
+    },
     container:{
-        marginTop: 50,
+        marginTop: 15,
         alignItems: 'center'},
     text:{
         padding: 15,
-        fontSize: 25,
+        fontSize: 20,
         margin: 5,
         textAlign: "center"},
     button:{
