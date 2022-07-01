a61dcf1442f349dbe80448845f4487f2a0a4af98
BINQIU1220 <ezio12a@hotmail.com>
2022-07-01 12:58:22 +0100

    tidies up codes and eliminated TS error redlines

--- assets/components/LoginModal.tsx
+++ assets/components/LoginModal.tsx
@@ -1,77 +1,84 @@
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Modal } from "react-native"
-import { useState, useEffect } from "react"
-import { auth } from "./firebase/config"
-import { reauthenticateWithCredential , EmailAuthProvider, signInWithPopup } from 'firebase/auth';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
+import { useState } from "react";
+import { reauthenticateWithCredential, EmailAuthProvider } from "firebase/auth";
 
-export default function LoginModal(){
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [modalVisible, setModalVisible] = useState(true);
+export default function LoginModal(props: any) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [modalVisible, setModalVisible] = useState(true);
 
-    function reauth() {
-        const credential = EmailAuthProvider.credential(email, password);
-        reauthenticateWithCredential(auth.currentUser, credential).then(() => {
-            setModalVisible(false)
-        }).catch((error) => {
-            alert(error)
-        })
-    }
+  function reauth() {
+    const credential = EmailAuthProvider.credential(email, password);
+    reauthenticateWithCredential(props.user, credential)
+      .then(() => {
+        setModalVisible(false);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
 
-    return (
-        <Modal
-            animationType="slide"
-            transparent={true}
-            visible={modalVisible}>
-                <View style={styles.centeredView}>
-                <View style={styles.modalView}>
-                    <Text style={styles.modalText}>Please log in again</Text>
-                    <TextInput
-                    style={styles.modalText}
-                    autoCapitalize='none'
-                    placeholder='email'
-                    onChangeText={setEmail}
-                    value={email}
-                    />
-                    <TextInput
-                    style={styles.modalText}
-                    autoCapitalize='none'
-                    secureTextEntry={true}
-                    placeholder='password'
-                    onChangeText={setPassword}
-                    value={password}
-                    />
-                    <TouchableOpacity onPress={reauth}><Text>Submit</Text></TouchableOpacity>
-                </View>
-                </View>
-        </Modal> 
-    )
+  return (
+    <Modal animationType="slide" transparent={true} visible={modalVisible}>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>Please log in again</Text>
+          <TextInput
+            style={styles.modalText}
+            autoCapitalize="none"
+            placeholder="email"
+            onChangeText={setEmail}
+            value={email}
+          />
+          <TextInput
+            style={styles.modalText}
+            autoCapitalize="none"
+            secureTextEntry={true}
+            placeholder="password"
+            onChangeText={setPassword}
+            value={password}
+          />
+          <TouchableOpacity onPress={reauth}>
+            <Text>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
 }
 
 const styles = StyleSheet.create({
-    centeredView: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 22
-      },
-    modalView: {
-        justifyContent: "center",
-        backgroundColor: "white",
-        borderRadius: 20,
-        paddingHorizontal: 100,
-        paddingVertical: 80,
-        alignItems: "center",
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5
-      },
-      modalText: {
-        marginBottom: 15,
-        textAlign: "center"
-      }
-})
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderRadius: 20,
+    paddingHorizontal: 100,
+    paddingVertical: 80,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+});
--- assets/components/UserAccount.tsx
+++ assets/components/UserAccount.tsx
@@ -1,132 +1,133 @@
-import { auth } from './firebase/config'
-import { updateEmail, updateProfile, reauthenticateWithCredential , EmailAuthProvider, signInWithPopup } from 'firebase/auth';
-import { Alert, View, Text, StyleSheet, TextInput, TouchableOpacity, Modal } from "react-native"
-import { useState, useEffect } from "react"
-import LoginModal from "./LoginModal"
+import { auth } from "./firebase/config";
+import { updateEmail, updateProfile } from "firebase/auth";
+import {
+  Alert,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useState, useEffect } from "react";
+import LoginModal from "./LoginModal";
 
-export default function UserAccount(){
-    interface userConfig {
-        createdAt: any,
-        displayName: string,
-        email: string,
-        phoneNumber?: number,
-        photoURL?: string,
+export default function UserAccount() {
+  const [user, setUser] = useState<any | null>({});
+  const [updatedEmail, setUpdatedEmail] = useState("");
+  const [updatedUsername, setUpdatedUsername] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [createdAt, setCreatedAt] = useState("");
+  const [modalVisible, setModalVisibile] = useState(false);
+
+  useEffect(() => {
+    setUser(auth.currentUser);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (user.metadata) {
+      const date = new Date(parseInt(user.metadata.createdAt));
+      setCreatedAt(
+        `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+      );
     }
+  }, [user]);
 
-    const [user, setUser] = useState<any | null>({});
-    const [update, setUpdate] = useState(false);
-    const [updatedEmail, setUpdatedEmail] = useState('');
-    const [updatedUsername, setUpdatedUsername] = useState('');
-    const [loading, setLoading] = useState(true);
-    const [createdAt , setCreatedAt] = useState('');
-    const [modalVisible, setModalVisibile] = useState(false);
+  function updateUserEmail() {
+    updateEmail(user, updatedEmail)
+      .then(() => {
+        Alert.alert("Email updated");
+        setUpdatedEmail("");
+      })
+      .catch((error) => {
+        if (error.code === "auth/invalid-email") {
+          Alert.alert("Invalid email");
+        } else if (error.code === "auth/email-already-in-use") {
+          Alert.alert("Email already in use");
+        } else if (error.code === "auth/requires-recent-login") {
+          setModalVisibile(true);
+        } else {
+          alert(error);
+        }
+      });
+  }
 
-    useEffect(() =>{
-        setUser(auth.currentUser)
-        setLoading(false)
-    }, [])
+  function updateUserUsername() {
+    updateProfile(user, {
+      displayName: updatedUsername,
+    })
+      .then(() => {
+        Alert.alert("Username updated");
+        setUpdatedUsername("");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
 
-    useEffect(() =>{
-    if (user.metadata){
-        const date = new Date(parseInt(user.metadata.createdAt))
-        setCreatedAt(`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`)
-    }}, [user])
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
 
-    function updateUserEmail(){
-        updateEmail(auth.currentUser, updatedEmail).then(() => {
-            Alert.alert("Email updated")
-            setUpdatedEmail("")
-        }).catch((error) => {
-            if (error.code === "auth/invalid-email"){
-                Alert.alert("Invalid email")
-            } else if (error.code === "auth/email-already-in-use") {
-                Alert.alert("Email already in use")
-            } else if (error.code === "auth/requires-recent-login") {
-                setModalVisibile(true)
-            } else {
-                alert(error)
-            }
-        })
-    }
-
-    function updateUserUsername(){
-        updateProfile(auth.currentUser, {
-            displayName: updatedUsername
-        }).then(() => {
-            Alert.alert("Username updated")
-            setUpdatedUsername("")
-        }).catch((error) => {
-            alert(error)
-        })
-    }
-
-    if (loading){
-        return <Text>Loading...</Text>
-    }
-
-    return (
-        <View>
-            { modalVisible ? <LoginModal /> : null }
-            <Text style={styles.title}>Account Details</Text>
-            <Text style={styles.text}>Username:</Text>
-            <View style={{flexDirection: "row"}}>
-                <TextInput 
-                    style={styles.textInfo} 
-                    placeholder={user.displayName}
-                    onChangeText={setUpdatedUsername}
-                    value={updatedUsername}>
-                </TextInput>
-                <TouchableOpacity 
-                    style={styles.updateBtn} 
-                    onPress={updateUserUsername}>
-                <Text>Update</Text>
-                </TouchableOpacity>
-            </View>
-            <Text style={styles.text}>Registered email:</Text>
-            <View style={{flexDirection: "row"}}>
-                <TextInput 
-                    style={styles.textInfo} 
-                    placeholder={user.email}  
-                    onChangeText={setUpdatedEmail}
-                    value={updatedEmail}
-                ></TextInput>
-                <TouchableOpacity 
-                    style={styles.updateBtn} 
-                    onPress={updateUserEmail}>
-                <Text>Update</Text>
-                </TouchableOpacity>
-            </View>
-            <Text style={styles.text}>Member Since {createdAt}</Text>
-        </View>
-    ) 
+  return (
+    <View>
+      {modalVisible ? <LoginModal user={user} /> : null}
+      <Text style={styles.title}>Account Details</Text>
+      <Text style={styles.text}>Username:</Text>
+      <View style={{ flexDirection: "row" }}>
+        <TextInput
+          style={styles.textInfo}
+          placeholder={user.displayName}
+          onChangeText={setUpdatedUsername}
+          value={updatedUsername}
+        ></TextInput>
+        <TouchableOpacity style={styles.updateBtn} onPress={updateUserUsername}>
+          <Text>Update</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.text}>Registered email:</Text>
+      <View style={{ flexDirection: "row" }}>
+        <TextInput
+          style={styles.textInfo}
+          placeholder={user.email}
+          onChangeText={setUpdatedEmail}
+          value={updatedEmail}
+        ></TextInput>
+        <TouchableOpacity style={styles.updateBtn} onPress={updateUserEmail}>
+          <Text>Update</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.text}>Member Since {createdAt}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    title:{
-        textAlign: 'center',
-        fontSize: 25,
-        textDecorationLine: 'underline',
-    },
-    text:{
-        fontSize: 18,
-        textAlign: 'left',
-        marginLeft: 15,
-        margin: 10,
-    },
-    textInfo:{
-        flex: 1,
-        fontSize: 17,
-        marginLeft: 30,
-        textAlign: "left"
-    },
-    updateBtn:{
-        backgroundColor: '#9cbedb',
-        width: 70,
-        borderRadius: 15,
-        margin: 5,
-        marginRight: 30,
-        padding: 5,
-        alignItems: "center",
-        alignSelf: "flex-end"
-    }
-})
+  title: {
+    textAlign: "center",
+    fontSize: 25,
+    textDecorationLine: "underline",
+  },
+  text: {
+    fontSize: 18,
+    textAlign: "left",
+    marginLeft: 15,
+    margin: 10,
+  },
+  textInfo: {
+    flex: 1,
+    fontSize: 17,
+    marginLeft: 30,
+    textAlign: "left",
+  },
+  updateBtn: {
+    backgroundColor: "#9cbedb",
+    width: 70,
+    borderRadius: 15,
+    margin: 5,
+    marginRight: 30,
+    padding: 5,
+    alignItems: "center",
+    alignSelf: "flex-end",
+  },
+});
